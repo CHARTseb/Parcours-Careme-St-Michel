9d826d20b2533d91f66ef67c9ebf9d4a62161055
CHARTseb <chartseb@icloud.com>
2026-05-25 23:38:30 +0200

    Re

--- src/types.ts
+++ src/types.ts
@@ -2,9 +2,16 @@ export type DayEntry = {
   id: number;
   date: string; // YYYY-MM-DD
   titre: string;
+
   reference_biblique?: string;
+
   texte_biblique?: string;
+
   reflexion?: string;
+
   resolution?: string;
+
+  priere?: string | null;
+
   paroisse?: string | null;
 };
